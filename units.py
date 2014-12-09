d66ff72faa330f4d05f7d475877c3b74d8c1e640
Zach Berta-Thompson <zkbt@mit.edu>
2014-12-08 20:43:57 -0500

    Add zachopy.units, for my own modifcations to astropy.units

---
+++ units.py
@@ -0,0 +1,10 @@
+from astropy.units import *
+
+# from IAU 2009
+Rsun = Unit('Rsun', 696000*km)
+Rearth = Unit('Rearth', 6378.1366*km)
+Rjupiter = Unit('Rjupiter', 71492*km)
+
+Msun = Unit('Msun', 1.9884e30*kg)
+Mjupiter = Unit('Mjupiter', Msun/1.047348644e03)
+Mearth = Unit('Mearth', Msun/332946.0487)
