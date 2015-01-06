e71847a3fd115ef8c177300ca9c6ac947795a517
Zach Berta-Thompson <zkbt@mit.edu>
2015-01-06 09:15:04 -0500

    Add lots of cgs definitions to zachopy.units

--- units.py
+++ units.py
@@ -1,10 +1,42 @@
-from astropy.units import *
+import numpy as np
 
-# from IAU 2009
-Rsun = Unit('Rsun', 696000*km)
-Rearth = Unit('Rearth', 6378.1366*km)
-Rjupiter = Unit('Rjupiter', 71492*km)
+# times
+second = 1.0
+minute = 60.0*second
+hour = 60.0*minute
+day = 24.0*hour
+century = 36525.0
+year = century/100.0
 
-Msun = Unit('Msun', 1.9884e30*kg)
-Mjupiter = Unit('Mjupiter', Msun/1.047348644e03)
-Mearth = Unit('Mearth', Msun/332946.0487)
+# distances
+cm = 1.0
+m = 100.0*cm
+km = 1000.0*m
+Rsun = 696000*km
+Rearth = 6378.1366*km
+Rjupiter = 71492*km
+au = 149597870700.0*m
+
+# speeds
+c = 299792458.0*m/second
+
+# masses
+g = 1.0
+kg = 1000.0
+Msun = 1.9884e30*kg
+Mjupiter = Msun/1.047348644e03
+Mearth = Msun/332946.0487
+mp = 1.66053892e-27*kg
+
+# angles
+radian = 1.0
+degree = np.pi/180.0
+arcmin = degree/60.0
+arcsec = degree/60.0
+
+# temperatures
+K = 1.0
+k = 1.3806488e-23*m**2*kg/second**2/K
+
+# other
+G = 6.67428e-11*m**3/kg/second**2
