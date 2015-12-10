670b74db97af0d0e3d27e2bddc71d13d354f35b0
Zach Berta-Thompson <zkbt@mit.edu>
2015-12-10 14:39:14 -0500

    add transparency to cmap maker

--- cmaps.py
+++ cmaps.py
@@ -5,12 +5,12 @@ def name2color(name):
     """Return the 3-element RGB array of a given color name."""
     return co.hex2color(co.cnames[name])
 
-def one2another(bottom='white', top='red', N=256):
+def one2another(bottom='white', top='red', alphatop=1.0, alphabottom=1.0, N=256):
     rgb_bottom, rgb_top = name2color(bottom), name2color(top)
     r = np.linspace(rgb_bottom[0],rgb_top[0],N)
     g = np.linspace(rgb_bottom[1],rgb_top[1],N)
     b = np.linspace(rgb_bottom[2],rgb_top[2],N)
-    a = np.ones(N)
+    a = np.linspace(alphabottom, alphatop,N)
     colors = np.transpose(np.vstack([r,g,b,a]))
     cmap = co.ListedColormap(colors, name='{bottom}2{top}'.format(**locals()))
     return cmap
